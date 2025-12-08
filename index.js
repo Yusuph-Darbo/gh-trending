d6f967038e7171ea8ab8afd8291bba1b6584446c
Yusuph <yusuphdarbo@gmail.com>
2025-12-08 14:49:56 +0000

    added default values to arguements & date functionality

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
 #!/usr/bin/env node
 
+import minimist from "minimist"
 import { Octokit } from '@octokit/core'
 
-const duration = require('minimist')(process.argv.slice(2))
-console.log(duration)
+// Returns a copy of the arguements starting from index 2 (inclusive)
+const duration = minimist(process.argv.slice(2), {
+    alias : {
+        d : "duration",
+        l : "limit",
+    },
 
+    // If options are omitted
+    default : {
+        duration : "daily",
+        limit : 20,
+    },
 
-console.log(duration)
-
+    unknown: (arg) => {
+    if (arg.startsWith("-")) {
+      console.error(`❌ Unknown option: ${arg}`)
+      process.exit(1)
+        }
+    }
+})
 const octokit = new Octokit()
+const date = new Date
+console.log(date.toLocaleTimeString("en-eu", {timeStyle: "medium"}))
 
-// const res = await octokit.request('GET /search/repositoriesq=stars:>10000&sort=stars&order=desc', {
+console.log(duration)
+getTimeFrame()
+
+// const res = await octokit.request('GET /search/repositories', {
+//     q: "stars:>5000 pushed:>2024-01-01",
+//     sort: "stars",
+//     order: "desc",
+//     per_page: duration.limit,
 //   headers: {
 //     'X-GitHub-Api-Version': '2022-11-28'
 //   }
 // })
 
 // console.log(res.data)
+
+function getTimeFrame(){
+    switch(duration.duration){
+
+         case 'year':
+            console.log("year")
+            break
+
+        case 'month':
+            console.log("month")
+            break
+
+        case 'week':
+            console.log("week")
+            break
+        
+        case 'day':
+            console.log("day")
+            break
+        
+        default:
+            console.error("Invalid input. Try again")
+            process.exit(2)
+    }
+}
