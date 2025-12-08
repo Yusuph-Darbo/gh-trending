e9624490b7f8454ab5d5187c3ab4b7248636d385
Yusuph <yusuphdarbo@gmail.com>
2025-12-08 13:08:00 +0000

    imported relevant modules

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import { Octokit } from '@octokit/core'
 
 const octokit = new Octokit()
 
+await octokit.request("GET search/repositories?q=stars:>5000&sort=stars&order=desc")
+
+console.log(octokit)
